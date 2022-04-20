c23bb941d43420bbc580f4a4e262ecc924e305c8
Curtion <curtion@126.com>
2022-04-20 12:02:23 +0800

    chore: 添加SQLite

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './SQLite/data.sql',
+      autoLoadEntities: true,
+    }),
+    UserModule,
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './dto/user.dto';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
   register(user: User) {
-    console.log(user);
+    this.userRepo.save(user);
     return user;
   }
 }
