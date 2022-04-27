5a05ff3170ba25dc89b0e2230c5b69e48e9c2f18
Curtion <curtion@126.com>
2022-04-27 23:24:07 +0800

    chore: workspace chore

--- src/modules/workpace/workpace.controller.ts
+++ src/modules/workpace/workpace.controller.ts
@@ -1,18 +1,1 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { UserService } from './workpace.service';
-import { UserDTO } from './dto/workpace.dto';
-
-@Controller('user')
-export class UserController {
-  constructor(private readonly userService: UserService) {}
-
-  @Post('register')
-  register(@Body() user: UserDTO) {
-    return this.userService.register(user);
-  }
-
-  @Post('login')
-  login(@Body() user: UserDTO) {
-    return this.userService.login(user);
-  }
-}
+export class WorkspaceController {}
--- src/modules/workpace/workpace.module.ts
+++ src/modules/workpace/workpace.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './entity/workpace.entity';
-import { UserController } from './workpace.controller';
-import { UserService } from './workpace.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
-  controllers: [UserController],
-  providers: [UserService],
+  imports: [],
+  controllers: [],
+  providers: [],
 })
-export class UserModule {}
+export class WorkspaceModule {}
